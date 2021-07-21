153cc322382769fe45b9acfb3aa8bca9309e627b
Dany Sluijk <me@dany.dev>
2021-07-22 00:46:18 +0200

    feat: add dockerfiles

--- accounts-http/src/main.rs
+++ accounts-http/src/main.rs
@@ -6,7 +6,7 @@ mod routes;
 /// Start the http server.
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let client = accounts::Client::connect("http://[::1]:50051")
+    let client = accounts::Client::connect("http://accounts:50051")
         .await
         .expect("Failed to connect to the accounts service.");
 
